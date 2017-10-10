08fecd91b5b37575e3520827ae1802806c491b8a
Phagun Baya <phagun.baya@gmail.com>
2017-10-10 12:28:39 -0700

    releasing v1.0.2

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='falkonry-cli',
-    version='1.0.1',
+    version='1.0.2',
     author='Falkonry Inc',
     author_email='info@falkonry.com',
     license='MIT',
     url='https://github.com/Falkonry/falkonry-cli',
-    download_url = 'https://github.com/Falkonry/falkonry-cli/tarball/1.0.1',
+    download_url = 'https://github.com/Falkonry/falkonry-cli/tarball/1.0.2',
     description='Cli tool to access Condition Prediction APIs',
     long_description='Falkonry cli tool to access Condition Prediction APIs',
     py_modules=['falkonry'],
