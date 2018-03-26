a17edf16b4d627f038efc51e48f5f981e3cd2105
Jaipreet Singh <jaipreet.singh@falkonry.com>
2018-03-26 11:08:00 +0530

    refs Falkonry/tracker#1325 transcript based tests

--- tests/test_datastream.py
+++ tests/test_datastream.py
@@ -1,7 +1,7 @@
+import argparse
 import unittest
 from unittest.mock import Mock, patch, create_autospec
 from .context import falkonry as FalkonryCli
-# from .context.falkonry import REPL
 from falkonryclient import client as Falkonry
 from falkonryclient import schemas as Schemas
 import xmlrunner
@@ -14,7 +14,7 @@ class TestDatastream(unittest.TestCase):
 
     @patch('falkonry.check_login', return_value=True)
     @patch('falkonry._falkonry',)
-    def test_do_datastream_get_list_with_no_datastreams(self, mockClient, mockLoggedIn):
+    def test_do_datastream_get_list(self, mockClient, mockLoggedIn):
         mockClient.return_value = Mock(Falkonry(host=self.host, token=self.token, options={"header":"falkonry-cli"}))
         mockClient.get_datastreams = Mock()
         mockClient.get_datastreams.return_value = []
@@ -22,20 +22,30 @@ class TestDatastream(unittest.TestCase):
         mockClient.get_datastreams.assert_called_with()
 
 
+    # @patch('falkonry.check_login', return_value=True)
+    # @patch('falkonry._falkonry',)
+    # def test_do_datastream_get_list_with_datastreams(self, mockClient, mockLoggedIn):
+    #     mockClient.side_effect = Mock(Falkonry(host=self.host, token=self.token, options={"header":"falkonry-cli"}))
+    #     mockClient.return_value = Mock(Falkonry(host=self.host, token=self.token, options={"header":"falkonry-cli"}))
+    #     mockClient.get_datastreams = Mock()
+    #
+    #     # mockClient.get_datastreams.return_value = [
+    #     #     {'type': 'entities.Datastream', 'dataSource': {'type': 'STANDALONE'}, 'live': 'ON', 'createdBy': 'qymdt8l48yklrp', 'inputList': [], 'updateTime': 1521117664695,  'name': 'Live1',  'id': 'n8gpyrhpbwwry9', 'timePrecision': 'micro', 'streaming': True,},
+    #     #     {'type': 'entities.Datastream', 'dataSource': {'type': 'STANDALONE'}, 'live': 'ON', 'createdBy': 'qymdt8l48yklrp', 'inputList': [], 'updateTime': 1521117664695,  'name': 'Live1',  'id': 'n8gpyrhpbwwry9', 'timePrecision': 'micro', 'streaming': True,}
+    #     # ]
+    #
+    #     mockClient.get_datastreams.return_value = Mock(Falkonry(host=self.host, token=self.token, options={"header":"falkonry-cli"}).get_datastreams)
+    #     FalkonryCli.REPL.do_datastream_get_list(FalkonryCli.REPL(),'')
+    #     mockClient.get_datastreams.assert_called_with()
+
     @patch('falkonry.check_login', return_value=True)
     @patch('falkonry._falkonry',)
-    def test_do_datastream_get_list_with_datastreams(self, mockClient, mockLoggedIn):
-        mockClient.side_effect = Mock(Falkonry(host=self.host, token=self.token, options={"header":"falkonry-cli"}))
+    def test_do_datastream_get_by_id(self, mockClient, mockLoggedIn):
         mockClient.return_value = Mock(Falkonry(host=self.host, token=self.token, options={"header":"falkonry-cli"}))
-        mockClient.get_datastreams = Mock(Schemas.Datastream())
-        # mockClient.get_datastreams.return_value = [
-        #     {'type': 'entities.Datastream', 'dataSource': {'type': 'STANDALONE'}, 'live': 'ON', 'createdBy': 'qymdt8l48yklrp', 'inputList': [], 'updateTime': 1521117664695,  'name': 'Live1',  'id': 'n8gpyrhpbwwry9', 'timePrecision': 'micro', 'streaming': True,},
-        #     {'type': 'entities.Datastream', 'dataSource': {'type': 'STANDALONE'}, 'live': 'ON', 'createdBy': 'qymdt8l48yklrp', 'inputList': [], 'updateTime': 1521117664695,  'name': 'Live1',  'id': 'n8gpyrhpbwwry9', 'timePrecision': 'micro', 'streaming': True,}
-        # ]
-
-        mockClient.get_datastreams.return_value = Mock(Falkonry(host=self.host, token=self.token, options={"header":"falkonry-cli"}).get_datastreams)
-        FalkonryCli.REPL.do_datastream_get_list(FalkonryCli.REPL(),'')
-        mockClient.get_datastreams.assert_called_with()
+        mockClient.get_datastream_by_id = Mock()
+        # mockClient.get_datastream_by_id.return_value = []
+        FalkonryCli.REPL.do_datastream_get_by_id(FalkonryCli.REPL(),'--id')
+        mockClient.get_datastream_by_id.assert_called_with('id')
 
 
 if __name__ == '__main__':
