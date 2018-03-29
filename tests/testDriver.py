30e5312733d75650b9498859e23f95c38fbd3f9b
Jaipreet Singh <jaipreet.singh@falkonry.com>
2018-03-30 04:26:03 +0530

    refs Falknory/tracker#1325

--- tests/testDriver.py
+++ tests/testDriver.py
@@ -7,8 +7,9 @@ host = os.environ['FALKONRY_HOST_URL']
 token = os.environ['FALKONRY_TOKEN']
 falkonry = Falkonry(host,token)
 falkonry_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+path_test_transcripts = falkonry_path + "/tests/test_transcripts"
 
-files = os.listdir("{path}/tests/test_transcripts".format(path=falkonry_path))
+files = os.listdir(path_test_transcripts)
 
 if(len(files) > 1):
     subprocess.call("rm -r {path}/tests/test_transcripts/*.txt".format(path=falkonry_path),shell=True)
