5871e2797abe61fbc4045d795bd68ce97ae97f18
Phagun Baya <phagun.baya@gmail.com>
2018-06-07 18:59:55 -0700

    releasing v2.1.1

--- setup.py
+++ setup.py
@@ -2,19 +2,19 @@ from setuptools import setup
 
 setup(
     name='falkonry-cli',
-    version='2.1.0',
+    version='2.1.1',
     author='Falkonry Inc',
     author_email='info@falkonry.com',
     license='MIT',
     url='https://github.com/Falkonry/falkonry-cli',
-    download_url = 'https://github.com/Falkonry/falkonry-cli/tarball/2.1.0',
+    download_url = 'https://github.com/Falkonry/falkonry-cli/tarball/2.1.1',
     description='Cli tool to access Condition Prediction APIs',
     long_description='Falkonry cli tool to access Condition Prediction APIs',
     py_modules=['falkonry'],
     install_requires=[
         'cmd2==0.8.0',
         'pprint==0.1',
-        'falkonryclient==2.1.0'
+        'falkonryclient==2.1.1'
     ],
     entry_points='''
         [console_scripts]
